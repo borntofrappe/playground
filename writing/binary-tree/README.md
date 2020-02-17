83c3ef7758755740a8473d3fe95e65c95296e468
borntofrappe <borntofrappe@gmail.com>
2020-02-18 00:00:02 +0100

    add code drawing the maze

--- writing/binary-tree/README.md
+++ writing/binary-tree/README.md
@@ -5,3 +5,5 @@ Exploring the binary tree algorithm, as found in the book [Mazes for Programmers
 Hopefully, I'll find the words to describe the code of this repo.
 
 - `binary-tree-algorithm.js`, setting up a grid of cells, each with a field detailing which side to actually draw
+
+- `binary-tree-maze.js`, creating the necessary markup to draw the maze with SVG syntax
---
+++ writing/binary-tree/binary-tree-maze.js
@@ -0,0 +1,79 @@
+const size = 5;
+const grid = Array(size ** 2)
+  .fill('')
+  .map((value, i) => ({
+    row: Math.floor(i / size),
+    column: i % size,
+    walls: {
+      north: true,
+      east: true,
+      south: true,
+      west: true,
+    },
+  }));
+
+
+for (let i = 0; i < grid.length; i += 1) {
+  const cell = grid[i];
+  const { row, column } = cell;
+  const neighbors = [];
+  if (row > 0) {
+    neighbors.push([row - 1, column]);
+  }
+  if (column < size - 1) {
+    neighbors.push([row, column + 1]);
+  }
+
+  const index = Math.floor(Math.random() * neighbors.length);
+  const neighbor = neighbors[index];
+
+  if (neighbor) {
+    const [r, c] = neighbor;
+    // d3.stratify
+    grid[i].parent = `${r} ${c}`;
+    if (r < row) {
+      grid[i].walls.north = false;
+      grid[r * size + c].walls.south = false;
+    } else {
+      grid[i].walls.east = false;
+      grid[r * size + c].walls.west = false;
+    }
+  }
+}
+
+
+const width = 500;
+const height = 500;
+
+// SVG
+const h = width / size;
+const v = height / size;
+const strokeWidth = 5;
+
+const markup = `
+<svg viewBox="${-strokeWidth / 2} ${-strokeWidth / 2} ${width + strokeWidth} ${height + strokeWidth}" width="${width}" height="${height}">
+  <defs>
+    <path id="north" d="M 0 0 h ${h}" />
+    <path id="east" d="M ${h} 0 v ${v}" />
+    <path id="south" d="M 0 ${v} h ${h}" />
+    <path id="west" d="M 0 0 v ${v}" />
+  </defs>
+  <g fill="none" stroke="currentColor" stroke-width="${strokeWidth}" stroke-linecap="square">
+  ${grid.map(cell => {
+    const {column, row, walls} = cell;
+    const x = column * h;
+    const y = row * v;
+    const directions = Object
+      .entries(walls)
+      .reduce((acc, curr) => (curr[1] ? [...acc, curr[0]] : [...acc]), []);
+    return `
+      <g transform="translate(${x} ${y})">
+        ${directions.map(direction => `<use href="#${direction}"/>`).join("")}
+      </g>
+    `
+  }).join("")}
+  </g>
+</svg>
+`;
+
+document.body.innerHTML += markup;
