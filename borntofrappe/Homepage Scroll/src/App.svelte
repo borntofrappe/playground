6fa1dbdb164f2daacdf8ee0a0360ab62e464f2bb
borntofrappe <borntofrappe@gmail.com>
2020-02-19 23:06:11 +0100

    complete smooth scrolling

--- borntofrappe/Homepage Scroll/src/App.svelte
+++ borntofrappe/Homepage Scroll/src/App.svelte
@@ -1,199 +1,64 @@
 <script>
-  import Icons from "./Icons.svelte";
-  // for the blog the link is actually to a relative path
+  import Navigation from "./Navigation.svelte";
   const links = [
     {
       name: "blog",
-      href: "/blog"
+      copy: "I enjoy writing code. Here, I try my best explaining how.",
+      href: "/blog",
     },
     {
       name: "codepen",
+      copy: "Demo or it didn't happen.",
       href: "https://codepen.io/borntofrappe"
     },
     {
       name: "freecodecamp",
+      copy: "Where it all started, with HTML and CSS.",
       href: "https://www.freecodecamp.org/borntofrappe"
     },
     {
       name: "twitter",
+      copy: "People seem to appreciate my coding efforts. I try to keep up.",
       href: "https://twitter.com/borntofrappe"
     },
     {
       name: "github",
+      copy: "Public by default.",
       href: "https://github.com/borntofrappe"
     }
   ];
 
-  const size = 470;
-  const iconSize = 110;
+  const items = links.map(({name}) => name);
 
-  const { length } = links;
-
-  const rings = length + 1;
-  const ring = length + 1;
-  const rounds = Array(rings)
-    .fill("")
-    .map((v, indexRings) => {
-      const delay = indexRings * 0.1;
-      const translate = (size / 2.5 / rings) * (indexRings + 1);
-      const particles = ring * (indexRings + 1);
-
-      const round = Array(particles)
-        .fill("")
-        .map((v, indexRing) => {
-          const scale = (indexRings + 1) ** 0.4;
-          const angle = 360 / particles;
-          const rotate = indexRings % 2 === 0 ? angle / 2 + angle * indexRing : angle * indexRing;
-
-          return {
-            scale,
-            rotate
-          };
-        });
-
-      return {
-        delay,
-        translate,
-        round
-      };
-    });
 </script>
 
-<nav>
-  <!-- wrapping SVG -->
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-{size / 2} -{size / 2} {size} {size}" width="{size}" height="{size}">
-    <defs>
-      <!-- path elements describing the circles
-      path-c and path-cc are used to map the text around the smaller `path` element
-      the difference between the two boils down to the clockwise, counter-clockwise direction
-      -->
-      <path id="path" d="M 0 -32 a 32 32 0 0 1 0 64 32 32 0 0 1 0 -64" />
-      <path id="path-c" d="M 0 40 a 40 40 0 0 1 0 -80 40 40 0 0 1 0 80" />
-      <path id="path-cc" d="M 0 47 a 47 47 0 0 0 0 -94 47 47 0 0 0 0 94" />
+<Navigation id="navigation" {items} />
 
-      <!-- mask to show the text only as it exceeds the path element encircling the icons -->
-      <mask id="mask-text">
-        <!-- <rect x="-50" y="-50" width="100" height="100" fill="hsl(0, 0%, 100%)" /> -->
-        <use transform="scale(2)" href="#path" fill="hsl(0, 0%, 100%)" />
-        <use href="#path" fill="hsl(0, 0%, 0%)" />
-      </mask>
-
-      <!-- mask to hide the particles that would coincide with the icons -->
-      <mask id="mask-icons">
-        <rect x="-{size / 2}" y="-{size / 2}" width="{size}" height="{size}" fill="hsl(0, 0%, 100%)" />
-        <circle r="46" fill="hsl(0, 0%, 0%)" />
-
-        {#each links as link, i}
-        <g transform="rotate({360 / links.length * i}) translate(0 -{Math.floor(size / 3)}) rotate({360 / links.length * i * -1})">
-          <use href="#path" transform="scale(1.4)" fill="hsl(0, 0%, 0%)" />
-        </g>
-        {/each}
-      </mask>
-    </defs>
-
-    <!-- group describing the particles as a backdrop -->
-    <g mask="url(#mask-icons)">
-      <g class="loaded">
-        {#each rounds as {delay, translate, round, opacity}}
-        <g class="loading" style="animation-delay: {delay}s;">
-          {#each round as {scale, rotate}}
-          <g transform="rotate({rotate}) translate(0 {translate}) rotate(-{rotate})">
-            <circle r="1" transform="scale({scale})" />
-          </g>
-          {/each}
-        </g>
-        {/each}
-      </g>
-    </g>
-
-    <!-- circle and check mark
-    see **Loading Animation**
-    -->
-    <g class="loading">
-      <g fill="none" stroke="currentColor" stroke-width="10" stroke-linecap="round" stroke-linejoin="round">
-        <circle transform="scale(-1 1) rotate(-90)" r="46" stroke-width="8" pathLength="1" />
-        <path d="M -20 -0 l 15 15 25 -25" stroke-width="10" pathLength="1" />
-      </g>
-    </g>
-
-    <!-- group wrapping the different icons
-    by translating the icons away from the center and scaling this group, you show them as if moving from the center
-    -->
-    <g class="loaded">
-      <!-- wrap each icon in an anchor link to make the shape click-able and focus-able -->
-      {#each links as {name, href}, i}
-      <g transform="rotate({360 / links.length * i}) translate(0 -{Math.floor(size / 3)}) rotate({360 / links.length * i * -1})">
-        <a href="{href}" aria-label="{name}">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-{iconSize / 2} -{iconSize / 2} {iconSize} {iconSize}" width="{Math.floor(size / 3)}" height="{Math.floor(size / 3)}" x="-{Math.floor(size / 6)}" y="-{Math.floor(size / 6)}">
-            <g>
-              <use href="#path" stroke="currentColor" stroke-width="6" fill="none" />
-              <!-- rotate the text around the center -->
-              <g transform="rotate({360 / links.length * i})" mask="url(#mask-text)">
-                <g class="text">
-                  <text fill="currentColor" font-weight="bold" letter-spacing="1" text-anchor="middle" font-size="12">
-                    <textPath href="#{360 / links.length * i > 90 && 360 / links.length * i < 270 ? 'path-cc' : 'path-c'}" startOffset="50%">
-                      {name}
-                    </textPath>
-                  </text>
-                </g>
-              </g>
-
-              <!-- re-scale the icon inside the wrapping path element -->
-              <g transform="scale(0.35) translate(-{iconSize / 2} -{iconSize / 2})">
-                <Icons icon="{name}" />
-              </g>
-
-              <!-- overlapping circle to expand the click area -->
-              <circle r="50" opacity="0" />
-            </g>
-          </svg>
-        </a>
-      </g>
-      {/each}
-    </g>
-  </svg>
-</nav>
+<div>
+  <a href="#navigation">Go to top</a>
+  {#each links as {name, copy, href}}
+    <section id="{name}">
+      <h1>{name}</h1>
+      <p>{copy}</p>
+      <a {href}>More</a>
+    </section>
+  {/each}
+</div>
 
 <style>
-  nav {
+  :global(html) {
+    scroll-behavior: smooth;
+  }
+  section {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    --jump: 0.35s;
-    --ease-out-back: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    --ease-in-out-back: cubic-bezier(0.68, -0.55, 0.265, 1.55);
   }
-  svg {
-    max-width: 600px;
-    width: 100vmin;
-    height: auto;
-    display: block;
-    margin: auto;
-  }
-
-  /* for the hover/focus transition, update the color and scale of the icon */
-  a {
-    color: inherit;
-    transform: scale(0.85);
-    transition: color 0.35s linear, transform 0.35s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-    transition: color var(--jump) linear, transform var(--jump) var(--ease-in-out-back);
-    outline: none;
-    text-decoration: none;
-  }
-  a:hover,
-  a:focus {
-    color: hsl(340, 80%, 55%);
-    transform: scale(1);
-  }
-  /* scale the group wrapping the text element to also show the label on hover/focus */
-  a .text {
-    transform: scale(0.5);
-    transition: transform 0.35s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-    transition: transform var(--jump) var(--ease-out-back);
-  }
-  a:hover .text,
-  a:focus .text {
-    transform: scale(1);
+  div > a {
+    position: sticky;
+    top: 1rem;
+    right: 1rem;
   }
 </style>
