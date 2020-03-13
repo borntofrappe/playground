4bb2b2c93f1e1d2049d5dfeabf13884270b97184
borntofrappe <borntofrappe@gmail.com>
2020-03-13 23:40:06 +0100

    add demo for icon set script

--- demos/README.md
+++ demos/README.md
@@ -7,3 +7,4 @@ Demos for which I intend to write a blog post.
 | title                | brief                                             |
 | -------------------- | ------------------------------------------------- |
 | CSS Animation Paused | animation and the `animation-play-state` property |
+| SVG Icon Set         | a node script to create an icon set               |
---
+++ demos/SVG Icon Set/icon-set-svelte/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import getIcon from "./icons.js";
+
+  let icon;
+  let size = 100;
+</script>
+
+<style>
+  form {
+    line-height: 2;
+    margin: 1rem 0;
+  }
+  label {
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+  }
+  input {
+    font-size: 1.25rem;
+    font-size: inherit;
+    margin-left: 0.5rem;
+    font-family: inherit;
+    padding: 0.2rem 0.5rem;
+    border: 2px solid currentColor;
+    background: hsl(0, 0%, 100%);
+  }
+</style>
+
+<form e:preventDefault>
+  <label>
+    Name ("html", "css", "js")
+    <input type="text" bind:value="{icon}" />
+  </label>
+  <label>
+    Default size
+    <input type="number" bind:value="{size}" min="0" />
+  </label>
+</form>
+
+{@html getIcon(icon, size)}
