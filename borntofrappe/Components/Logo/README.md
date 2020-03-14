a6db39b7b0ab2d8c36563d18d6833553b4bdbb06
borntofrappe <borntofrappe@gmail.com>
2020-03-14 21:58:41 +0100

    design and animate borntofrappe logo

---
+++ borntofrappe/Components/Logo/README.md
@@ -0,0 +1,5 @@
+# Logo
+
+Here I design and animate the logo of the website.
+
+It is meant to substitute the default check mark introduced in the navigation project.
---
+++ borntofrappe/Components/Logo/script.js
@@ -0,0 +1,5 @@
+const pathB = document.querySelector('#b');
+const pathAccent = document.querySelector('#accent');
+
+console.log(pathB.getTotalLength()); // 130.55661010742188
+console.log(pathAccent.getTotalLength()); // 14.008037567138672
